be2f6c809779edbc1ce849a00a9e0e68ff41d4ca
Dani Lyn <dlyn1110@gmail.com>
2019-02-07 18:22:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ const returnLastTwoDrivers = function(driversArray) {
   return newArray.slice(-2);
 }
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (num) {
+    return (function (fare) {
+        return num * fare;
+    })
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arrayOfDrivers, eitherDrivers) {
+    return eitherDrivers(arrayOfDrivers);
+}
+    
